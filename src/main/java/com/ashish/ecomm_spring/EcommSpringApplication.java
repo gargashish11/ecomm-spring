f88ac1f4bda75c838ed3c0e97c6d011c8a55108d
Ashish Garg <gargashish11@gmail.com>
2022-05-16 21:34:46 +0530

    first commit

---
+++ src/main/java/com/ashish/ecomm_spring/EcommSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.ecomm_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashish/ecomm_spring/EcommSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.ecomm_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
